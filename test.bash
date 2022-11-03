03a9a60099c948519d104fee1d50a3ce445e1ee6
Ohashi822 <s21c1025qu@s.chibakoudai.jp>
2022-11-03 12:23:41 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2022 Kazuma_Ohashi
 # SPDX-License-Identifier: BSD-3-Clause
- printf(OK)
+
+out=$(seq 5 | ./plus)
+[ "$[out]" = 15 ] || ng $[LINENO]
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]     || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+ exit $res
