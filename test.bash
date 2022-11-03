9a96dd4d7fd5fe82818cc7a9e620324cc7f1d622
Ohashi822 <s21c1025qu@s.chibakoudai.jp>
2022-11-03 12:30:32 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,8 +2,13 @@
 # SPDX-FileCopyrightText: 2022 Kazuma_Ohashi
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
 out=$(seq 5 | ./plus)
-[ "$[out]" = 15 ] || ng $[LINENO]
+[ "${out}" = 15 ] || ng $[LINENO]
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]       || ng ${LINENO}
